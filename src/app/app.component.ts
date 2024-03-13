afd94e7c71289d953c01a8026f17f9488335a0dc
johannroll <johannroll@gmail.com>
2024-03-13 18:38:10 +0200

    dist rm from git ignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    CommonModule
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+
   title = 'FirstApp';
+
+  selectedTheme: string = '';
+
+  themes: string[] = [
+    "light",
+    "dark",
+    "cupcake",
+    "bumblebee",
+    "emerald",
+    "corporate",
+    "synthwave",
+    "retro",
+    "cyberpunk",
+    "valentine",
+    "halloween",
+    "garden",
+    "forest",
+    "aqua",
+    "lofi",
+    "pastel",
+    "fantasy",
+    "wireframe",
+    "black",
+    "luxury",
+    "dracula",
+    "cmyk",
+    "autumn",
+    "business",
+    "acid",
+    "lemonade",
+    "night",
+    "coffee",
+    "winter",
+    "dim",
+    "nord",
+    "sunset",
+  ];
+
+  onThemeChange(event: any): void {
+    this.selectedTheme = event.target.value;
+    console.log('Selected theme:', this.selectedTheme);
+    const htmlTag = document.getElementById("html");
+    console.log(htmlTag);
+    htmlTag?.setAttribute('data-theme', this.selectedTheme);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent}
+];
